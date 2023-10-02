a5774caa30b2ba71c3333bc1eadb4cdc30e2ad9c
CaosEdd <kadujunior1997@gmail.com>
2023-10-02 18:38:10 -0300

    updade 2

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,7 +1,10 @@
-﻿namespace LanchesMac.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models;
 
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
     public string CategoriaNome { get; set; }
     public string Descricao { get; set; }
--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LanchesMac.Models;
@@ -22,7 +23,9 @@ public class Lanche
     public string DescricaoDetalhada { get; set; }
 
     [NotMapped]
-    public DateTime DataDeCriacao {get; set;}    
+    public DateTime DataDeCriacao {get; set;}
+
+    [Precision(10,2)]
     public decimal Preco { get; set; }
     public string ImagemUrl { get; set; }
     public string ImagemThumbnailUrl { get; set; }
