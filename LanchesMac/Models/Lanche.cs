74a3b54d691cf3cf7b48c8864a965b326fc6c97f
CaosEdd <kadujunior1997@gmail.com>
2023-10-02 18:37:57 -0300

    update 1

---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models;
+
+[Table("Lanches")]
+public class Lanche
+{
+    [Key]
+    public int LancheId { get; set; }
+    
+    [Required(ErrorMessage = "O nome d lanche deve ser informado")]
+    [Display(Name = "Nome do Lanche")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+    [Display(Name = "Descrição do lanche")]
+    [MinLength(20, ErrorMessage = "Descrição deve conter no minimo {1} caracteres")]
+    [MaxLength(200, ErrorMessage = "Descrição não deve exceder {1} caracteres")]
+    public string DescricaoCurta { get; set;}
+
+    public string DescricaoDetalhada { get; set; }
+
+    [NotMapped]
+    public DateTime DataDeCriacao {get; set;}    
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+    public string ImagemThumbnailUrl { get; set; }
+    public string IsLanchePreferido { get; set; }
+    public bool EmEstoque { get; set; }
+
+
+    public int CategoriaId { get; set; } 
+    public virtual Categoria Categoria { get; set; }
+
+}
+
