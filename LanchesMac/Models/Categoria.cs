ae8971017ba63e8670f93563b20ff5948564a3a0
CaosEdd <kadujunior1997@gmail.com>
2023-10-02 19:45:28 -0300

    Alterado as data anotations da classe categoria

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,12 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LanchesMac.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
     [Key]
     public int CategoriaId { get; set; }
+
+    [StringLength(100, ErrorMessage = "O tamanho não pode ser maior que {1} caracteres")]
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [Display(Name = "Nome")]
     public string CategoriaNome { get; set; }
+
+    [StringLength(200, ErrorMessage = "O tamanho não pode ser maior que {1} caracteres")]
+    [Required(ErrorMessage = "Informe a descrição")]
+    [Display(Name = "Descrição")]
     public string Descricao { get; set; }
 
 
